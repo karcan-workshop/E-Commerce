4aa1de3625c4a1ccd1b1f99d15447c3d80ddbe05
Karcan Ozbal <karcan.workshop@hotmail.com>
2023-10-25 19:46:33 +0300

    Product row count increased to 1m for per category

--- E-Commerce.DB/Scripts/Post Deployments/Data/Products.sql
+++ E-Commerce.DB/Scripts/Post Deployments/Data/Products.sql
@@ -1,13 +1,15 @@
 -- Write your own SQL object definition here, and it'll be included in your package.
-INSERT INTO dbo.Products (CategoryID, ModelID, Name, CreateUserID)
+SET QUOTED_IDENTIFIER ON;
+
+INSERT INTO [dbo].[Products] ([CategoryID], [ModelID], [Name], [CreateUserID])
 SELECT 
         c1.ID
     ,   m.ID
-    ,   CONCAT(c1.Name, N' - ', FORMAT(v.[value], '000000'))
+    ,   CONCAT(c1.Name, N' - ', FORMAT(v.[value], '0000000'))
     ,   1
 FROM 
-    dbo.Categories AS c1
-    CROSS APPLY generate_series(1, 100000, 1) AS v
-    OUTER APPLY (SELECT TOP(1) ID FROM dbo.Models AS m ORDER BY NEWID(), CHECKSUM(v.[value])) AS m
+    [dbo].[Categories] AS c1
+    CROSS APPLY generate_series(1, 1000000, 1) AS v
+    OUTER APPLY (SELECT TOP(1) ID FROM [dbo].[Models] AS m ORDER BY NEWID(), CHECKSUM(v.[value])) AS m
 WHERE
-    NOT EXISTS (SELECT NULL FROM dbo.Categories AS c2 WHERE c2.ParentCategoryID = c1.ID)
+    NOT EXISTS (SELECT NULL FROM [dbo].[Categories] AS c2 WHERE c2.ParentCategoryID = c1.ID)
